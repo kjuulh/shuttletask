d4b409439a4dafb6c2b576a9b28d9f9b9ad876f2
kjuulh <contact@kjuulh.io>
2023-03-13 21:58:23 +0100

    refactor: split

--- pkg/compile/compile.go
+++ pkg/compile/compile.go
@@ -1,35 +1,19 @@
 package compile
 
 import (
-	"bytes"
 	"context"
-	"embed"
-	"encoding/hex"
-	"errors"
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"io"
 	"log"
-	"os"
-	"os/exec"
 	"path"
-	"strings"
-	"text/template"
 
+	"github.com/kjuulh/shuttletask/pkg/codegen"
+	"github.com/kjuulh/shuttletask/pkg/compile/matcher"
 	"github.com/kjuulh/shuttletask/pkg/discover"
-	cp "github.com/otiai10/copy"
-	"golang.org/x/mod/sumdb/dirhash"
-)
-
-var (
-	//go:embed templates/mainFile.tmpl
-	mainFileTmpl embed.FS
+	"github.com/kjuulh/shuttletask/pkg/parser"
+	"github.com/kjuulh/shuttletask/pkg/shuttlefolder"
 )
 
 const (
-	alwaysBuild = true
+	alwaysBuild = false
 )
 
 // discovered: Discovered shuttletask projects
@@ -53,17 +37,17 @@ func Compile(ctx context.Context, discovered *discover.Discovered) (string, erro
 }
 
 func compile(ctx context.Context, shuttletask *discover.ShuttleTaskDiscovered) (string, error) {
-	hash, err := getHash(ctx, shuttletask)
+	hash, err := matcher.GetHash(ctx, shuttletask)
 	if err != nil {
 		return "", err
 	}
 
-	binaryPath, ok, err := binaryMatches(ctx, hash, shuttletask)
+	binaryPath, ok, err := matcher.BinaryMatches(ctx, hash, shuttletask)
 	if err != nil {
 		return "", err
 	}
 
-	if ok && !alwaysBuild { //FIXME: remember to flip to ok
+	if ok && !alwaysBuild {
 		log.Printf("file already matches continueing\n")
 		// The binary is the same so we short circuit
 		return binaryPath, nil
@@ -71,247 +55,36 @@ func compile(ctx context.Context, shuttletask *discover.ShuttleTaskDiscovered) (
 
 	shuttlelocaldir := path.Join(shuttletask.ParentDir, ".shuttle/shuttletask")
 
-	// Generate tmp dir
-	if err = generateTmpDir(ctx, shuttlelocaldir); err != nil {
+	if err = shuttlefolder.GenerateTmpDir(ctx, shuttlelocaldir); err != nil {
 		return "", err
 	}
-	// Copy files
-	if err = copyFiles(ctx, shuttlelocaldir, shuttletask); err != nil {
+	if err = shuttlefolder.CopyFiles(ctx, shuttlelocaldir, shuttletask); err != nil {
 		return "", err
 	}
 
-	// Generate AST
-	contents, err := generateAst(ctx, shuttlelocaldir, shuttletask)
+	contents, err := parser.GenerateAst(ctx, shuttlelocaldir, shuttletask)
 	if err != nil {
 		return "", err
 	}
 
-	// Generate Main file
-	if err = generateMainFile(ctx, shuttlelocaldir, shuttletask, contents); err != nil {
+	if err = codegen.GenerateMainFile(ctx, shuttlelocaldir, shuttletask, contents); err != nil {
 		return "", err
 	}
 
-	// Compile package
-	if err = modTidy(ctx, shuttlelocaldir); err != nil {
+	if err = codegen.Format(ctx, shuttlelocaldir); err != nil {
 		return "", err
 	}
-	binarypath, err := compileBinary(ctx, shuttlelocaldir)
+
+	if err = codegen.ModTidy(ctx, shuttlelocaldir); err != nil {
+		return "", err
+	}
+	binarypath, err := codegen.CompileBinary(ctx, shuttlelocaldir)
 	if err != nil {
 		return "", err
 	}
-	// Move binary
-	finalBinaryPath := path.Join(
-		shuttlelocaldir,
-		"binaries",
-		fmt.Sprintf("shuttletask-%s", hex.EncodeToString([]byte(hash)[:16])),
-	)
-	os.Rename(binarypath, finalBinaryPath)
+
+	finalBinaryPath := shuttlefolder.CalculateBinaryPath(shuttlelocaldir, hash)
+	shuttlefolder.Move(binarypath, finalBinaryPath)
 
 	return finalBinaryPath, nil
-}
-
-type Function struct {
-	Name   string
-	Input  []Arg
-	Output Output
-}
-
-type Arg struct {
-	Name string
-}
-
-type Output struct {
-	Error bool
-}
-
-func generateAst(ctx context.Context, shuttlelocaldir string, shuttletask *discover.ShuttleTaskDiscovered) ([]*Function, error) {
-	funcs := make([]*Function, 0)
-
-	for _, taskfile := range shuttletask.Files {
-		tknSet := token.NewFileSet()
-		astfile, err := parser.ParseFile(tknSet, path.Join(shuttlelocaldir, "tmp", taskfile), nil, parser.ParseComments)
-		if err != nil {
-			return nil, err
-		}
-		if ast.FileExports(astfile) {
-			decls := astfile.Decls
-			for _, decl := range decls {
-				funcdecl, ok := decl.(*ast.FuncDecl)
-				if ok {
-					f := Function{}
-					f.Name = funcdecl.Name.Name
-					param := funcdecl.Type
-					paramList := param.Params.List
-					for _, param := range paramList {
-						for _, name := range param.Names {
-							if name != nil && !strings.Contains(fmt.Sprintf("%s", param.Type), "Context") {
-								f.Input = append(f.Input, Arg{
-									Name: name.Name,
-								})
-							}
-						}
-					}
-					outputParam := param.Results
-					if outputParam != nil {
-						if len(outputParam.List) > 1 {
-							return nil, errors.New("only error is supported as an output param")
-						}
-						if len(outputParam.List) == 0 {
-							return nil, errors.New("output params are required, only error is supported")
-						}
-
-						for _, param := range outputParam.List {
-							if fmt.Sprintf("%s", param.Type) != "error" {
-								return nil, errors.New("output was not error")
-							}
-						}
-
-						f.Output = Output{Error: true}
-					}
-
-					funcs = append(funcs, &f)
-				}
-			}
-		}
-	}
-
-	return funcs, nil
-}
-
-func modTidy(ctx context.Context, shuttlelocaldir string) error {
-	cmd := exec.Command("go", "mod", "tidy")
-	cmd.Dir = path.Join(shuttlelocaldir, "tmp")
-
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("%s\n", string(output))
-		return err
-	}
-
-	return nil
-}
-
-func compileBinary(ctx context.Context, shuttlelocaldir string) (string, error) {
-	cmd := exec.Command("go", "build")
-	cmd.Dir = path.Join(shuttlelocaldir, "tmp")
-
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("%s\n", string(output))
-		return "", err
-	}
-
-	return path.Join(shuttlelocaldir, "tmp", "shuttletask"), nil
-}
-
-func generateMainFile(
-	ctx context.Context,
-	shuttlelocaldir string,
-	shuttletask *discover.ShuttleTaskDiscovered,
-	functions []*Function,
-) error {
-	tmpmainfile := path.Join(shuttlelocaldir, "tmp/main.go")
-
-	file, err := os.Create(tmpmainfile)
-	if err != nil {
-		return err
-	}
-
-	tmpl := template.Must(template.New("mainFile.tmpl").Funcs(map[string]any{
-		"lower": strings.ToLower,
-	}).ParseFS(mainFileTmpl, "templates/mainFile.tmpl"))
-
-	err = tmpl.Execute(file, functions)
-
-	return err
-}
-
-func copyFiles(
-	ctx context.Context,
-	shuttlelocaldir string,
-	shuttletask *discover.ShuttleTaskDiscovered,
-) error {
-	tmpdir := path.Join(shuttlelocaldir, "tmp")
-
-	return cp.Copy(shuttletask.DirPath, tmpdir)
-}
-
-func generateTmpDir(ctx context.Context, shuttlelocaldir string) error {
-	if err := os.MkdirAll(shuttlelocaldir, 0755); err != nil {
-		return err
-	}
-
-	binarydir := path.Join(shuttlelocaldir, "binaries")
-	if err := os.RemoveAll(binarydir); err != nil {
-		return nil
-	}
-	if err := os.MkdirAll(binarydir, 0755); err != nil {
-		return err
-	}
-
-	tmpdir := path.Join(shuttlelocaldir, "tmp")
-	if err := os.RemoveAll(tmpdir); err != nil {
-		return nil
-	}
-	if err := os.MkdirAll(tmpdir, 0755); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func binaryMatches(
-	ctx context.Context,
-	hash string,
-	shuttletask *discover.ShuttleTaskDiscovered,
-) (string, bool, error) {
-	shuttlebindir := path.Join(shuttletask.ParentDir, ".shuttle/shuttletask/binaries")
-
-	if _, err := os.Stat(shuttlebindir); errors.Is(err, os.ErrNotExist) {
-		log.Println("DEBUG: package doesn't exist continueing")
-		return "", false, nil
-	}
-
-	entries, err := os.ReadDir(shuttlebindir)
-	if err != nil {
-		return "", false, err
-	}
-
-	if len(entries) == 0 {
-		return "", false, err
-	}
-
-	log.Printf("%s", entries[0].Name())
-	// We only expect a single binary in the folder, so we just take the first entry if it exists
-	binary := entries[0]
-
-	if binary.Name() == fmt.Sprintf("shuttletask-%s", hex.EncodeToString([]byte(hash)[:16])) {
-		return path.Join(shuttlebindir, binary.Name()), true, nil
-	} else {
-		return "", false, nil
-	}
-}
-
-func getHash(ctx context.Context, shuttletask *discover.ShuttleTaskDiscovered) (string, error) {
-	entries := make([]string, len(shuttletask.Files))
-
-	for i, task := range shuttletask.Files {
-		entries[i] = path.Join(shuttletask.DirPath, task)
-	}
-
-	open := func(name string) (io.ReadCloser, error) {
-		b, err := os.ReadFile(name)
-		if err != nil {
-			return nil, err
-		}
-
-		return io.NopCloser(bytes.NewReader(b)), nil
-	}
-
-	hash, err := dirhash.Hash1(entries, open)
-	if err != nil {
-		return "", err
-	}
-
-	return hash, nil
 }
