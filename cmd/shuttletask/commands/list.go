8373d0e75edccbd64718b75c8c09a9bba367543b
kjuulh <contact@kjuulh.io>
2023-03-17 10:20:19 +0100

    feat: list function

--- cmd/shuttletask/commands/list.go
+++ cmd/shuttletask/commands/list.go
@@ -1,11 +1,7 @@
 package commands
 
 import (
-	"log"
-	"os/exec"
-
-	"github.com/kjuulh/shuttletask/pkg/compile"
-	"github.com/kjuulh/shuttletask/pkg/discover"
+	"github.com/kjuulh/shuttletask/pkg/executer"
 	"github.com/spf13/cobra"
 )
 
@@ -16,21 +12,8 @@ func ListCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 
-			disc, err := discover.Discover(ctx, "shuttle.yaml")
-			if err != nil {
+			if err := executer.List(ctx, "shuttle.yaml"); err != nil {
 				return err
-			}
-
-			binary, err := compile.Compile(ctx, disc)
-			if err != nil {
-				return err
-			}
-
-			execmd := exec.Command(binary.Local.Path, "ls")
-			output, err := execmd.CombinedOutput()
-			log.Printf("%s\n", string(output))
-			if err != nil {
-				log.Fatal(err)
 			}
 
 			return nil
